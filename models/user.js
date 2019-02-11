352b96d4a54e2182d10808898bf04cda836a6a94
957196958@qq.com <957196958@qq.com>
2019-02-11 16:37:32 +0800

    用户登录日志

--- models/user.js
+++ models/user.js
@@ -1,43 +1,65 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username:{ type: String },
-  password: { type: String },
-  authority:{ type: Number, default:0 }, //权限 默认0普通user   1 admin   2 禁止留言,评论  3 禁止登录
-  email:{type: String, unique: true},
-  avatar: { type: String,default:"/images/default.jpg"},
-  lastLoginTime:[]
-},{
-  collection: 'users',
-  versionKey: false
-});
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String },
+    password: { type: String },
+    authority: { type: Number, default: 0 }, //权限 默认0普通user   1 admin   2 禁止留言,评论  3 禁止登录
+    email: { type: String, unique: true },
+    avatar: { type: String, default: "/images/default.jpg" },
+    lastLoginTime: []
+  },
+  {
+    collection: "users",
+    versionKey: false
+  }
+);
 
-let userModel = mongoose.model('user',userSchema);
+let userModel = mongoose.model("user", userSchema);
 
-userModel.addUser = (user) =>{
-  return user.save({}); 
-}
-userModel.delete =(email) => {
-  return userModel.remove({email})
-}
-userModel.get =() => {
+userModel.addUser = user => {
+  return user.save({});
+};
+userModel.delete = email => {
+  return userModel.remove({ email });
+};
+userModel.get = () => {
   return userModel.find({});
-}
-userModel.findUser = (user) => {
-  return userModel.findOne(user,{ password:0 }) //0不显示 1显示
-}
-userModel.updateUser = (id,power) => {
+};
+userModel.findUser = user => {
+  return userModel.findOne(user, { password: 0 }); //0不显示 1显示
+};
+userModel.updateUser = (id, power) => {
   return userModel.findOneAndUpdate(
     { _id: id },
     { $set: { authority: power } },
     { new: true }
-  )
-}
-userModel.makeRecords = (email,records) => {
-  return userModel.findOneAndUpdate(
-    { email:email },
-    { $push: { lastLoginTime: records  } },
-    { new: true }
-  )
-}
+  );
+};
+userModel.makeRecords = async (email, records) => {
+  //只保存最近30次的登录记录
+  let data = await userModel.aggregate([
+    { $match: { email } },
+    { $project: { count: { $size: "$lastLoginTime" } } }
+  ]);
+  return data[0].count >= 30
+    ? userModel
+        .findOneAndUpdate(
+          { email: email },
+          { $pop: { lastLoginTime: -1 } },
+          { new: true }
+        )
+        .then(() => {
+          userModel.findOneAndUpdate(
+            { email: email },
+            { $push: { lastLoginTime: records } },
+            { new: true }
+          );
+        })
+    : userModel.findOneAndUpdate(
+        { email: email },
+        { $push: { lastLoginTime: records } },
+        { new: true }
+      );
+};
 export default userModel;
